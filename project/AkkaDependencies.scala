a082916621baf46627162fd89f6774bfc15f41ec
Ralph Schäfer <raschaef@gmx.de>
2022-11-15 20:10:06 +0100

    cleanup dependencies

--- project/AkkaDependencies.scala
+++ project/AkkaDependencies.scala
@@ -1,33 +1,13 @@
 import sbt._
 
 object AkkaDependencies {
-  lazy val akkaHttpVersion = "10.2.0"
-  lazy val akkaVersion    = "2.6.6"
+  lazy val akkaHttpVersion = "10.4.0"
+  lazy val akkaVersion    = "2.7.0"
   lazy val akkaManagementVersion = "1.0.8"
 
   lazy val libs = Seq (
-      "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-      "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
-      "com.typesafe.akka" %% "akka-cluster-typed" % akkaVersion,
-      "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
-      "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion,
-      "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
-      "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
-      "com.typesafe.akka" %% "akka-http-xml" % akkaHttpVersion,
-      "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
-      "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-      "com.typesafe.akka" %% "akka-discovery" % akkaVersion,
-      "com.typesafe.akka" %% "akka-distributed-data" % akkaVersion,
       "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-      "com.lightbend.akka.management" %% "akka-management" % akkaManagementVersion,
-      "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % akkaManagementVersion,
-      // "com.lightbend.akka.discovery" %% "akka-discovery-dns" % akkaManagementVersion,
-      "com.lightbend.akka.management" %% "akka-management-cluster-http" % akkaManagementVersion,
-      "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
-      "org.json4s" %% "json4s-jackson" % "3.6.10",
       "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-      "ch.qos.logback" % "logback-classic" % "1.2.3",
-      "com.twitter" %% "chill-akka" % "0.9.5",
 
       "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
       "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
